ad205f0ea8ba0c7bd81454ecea6c9ae8480aae56
sharmatriloknath <sharmatrilonath336@gmail.com>
2023-07-27 15:38:56 +0530

    Added PICONE vector Database

--- Ask From Multiple Pdfs/app.py
+++ Ask From Multiple Pdfs/app.py
@@ -1,14 +1,23 @@
 import streamlit as st
+from streamlit_chat import message
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS
+# from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
+# from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+
+#IMPORT PINECONE
+import os
+from langchain.vectorstores import Pinecone
+import pinecone
+
+INDEX_NAME = "electrical-vehicles-details-index"
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -31,24 +40,33 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
+
+    # initialize picone vector database
+    pinecone.init(api_key=os.getenv("PICONE_API_KEY"), 
+                  environment=os.getenv("PINECONE_ENVIRONMENT"))
+    
+    print("available indexes:",pinecone.list_indexes())
+
     # embeddings = OpenAIEmbeddings()
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
+    # vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    Pinecone.from_texts(texts=text_chunks, embedding=embeddings, index_name=INDEX_NAME)
+    print("Creating Embeddings........")
 
-
-def get_conversation_chain(vectorstore):
-    # llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
+def run_llm(query,chat_history):
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    docsearch = Pinecone.from_existing_index(
+        embedding=embeddings,
+        index_name=INDEX_NAME,
     )
-    return conversation_chain
+    llm = HuggingFaceHub(repo_id="google/flan-ul2", model_kwargs={"temperature":0.5, "max_length":512},verbose=True)
+    
+    conversational_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm, retriever=docsearch.as_retriever(), return_source_documents=True
+    )
+
+
+    return conversational_chain({"question": query, "chat_history": chat_history})
 
 
 def handle_userinput(user_question):
@@ -66,26 +84,33 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
+    
+    # initialize picone vector database
+    pinecone.init(api_key=os.getenv("PICONE_API_KEY"), 
+                  environment=os.getenv("PINECONE_ENVIRONMENT"))
+    
+    print("available indexes:",pinecone.list_indexes())
+
+
     st.set_page_config(page_title="Ask About Electrical Vehicles",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+    if (
+    "chat_answers_history" not in st.session_state
+    and "user_prompt_history" not in st.session_state
+    and "chat_history" not in st.session_state):
+        st.session_state["chat_answers_history"] = []
+        st.session_state["user_prompt_history"] = []
+        st.session_state["chat_history"] = []
 
-    st.header("Ask About Electrical Vehicles")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        handle_userinput(user_question)
-
+    # Create a Vectorstore
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader(
             "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
-            with st.spinner("Processing"):
+            with st.spinner("In Progress...."):
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
 
@@ -93,12 +118,30 @@ def main():
                 text_chunks = get_text_chunks(raw_text)
 
                 # create vector store
-                vectorstore = get_vectorstore(text_chunks)
+                get_vectorstore(text_chunks)
+    
+    st.header("Ask About Electrical Vehicles")
+    prompt = st.text_input("Enter You Question", placeholder="Enter your message here...") or st.button("Submit")
+    if prompt:
+        # handle_userinput(prompt)
+        with st.spinner("Generating response..."):
+            generated_response = run_llm(
+                query=prompt, chat_history=st.session_state["chat_history"]
+            )
+            st.session_state.chat_history.append((prompt, generated_response["answer"]))
+            st.session_state.user_prompt_history.append(prompt)
+            st.session_state.chat_answers_history.append(generated_response["answer"])
 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
-
+        if st.session_state["chat_answers_history"]:
+            for generated_response, user_query in zip(
+                st.session_state["chat_answers_history"],
+                st.session_state["user_prompt_history"],
+            ):
+                message(
+                    user_query,
+                    is_user=True,
+                )
+                message(generated_response)
 
 if __name__ == '__main__':
     main()
